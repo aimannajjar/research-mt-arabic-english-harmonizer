3e9c6dcd28472f1c1efbd6db9779196cd7583b86
Yasser Aboudkhil <yasser.oompaloompa@gmail.com>
2013-04-22 04:49:24 -0400

    Fixed small bug in PhraseTableDataExtractor.java dealing with lemmas containing a ','

--- morphTaze/PhraseTableDataExtractor.java
+++ morphTaze/PhraseTableDataExtractor.java
@@ -8,8 +8,12 @@ public class PhraseTableDataExtractor {
 
 		// lemma | features ||| in europe ||| 0.829007 0.207955 0.801493 0.492402 2.718
 		String file = args[0];
+		String outTrainFile = args[1];
+		String outClassMappingsFile = args[2];
 		Scanner input = new Scanner(new GZIPInputStream(new FileInputStream(file)));
 		HashMap<String, TreeSet<String>> prs_tbl = new HashMap<String, TreeSet<String>>();
+		FileWriter fw1 = new FileWriter(outTrainFile);
+		FileWriter fw2 = new FileWriter(outClassMappingsFile);
 		
 		while(input.hasNextLine()) {
 
@@ -23,13 +27,9 @@ public class PhraseTableDataExtractor {
 			String[] scores = line[2].trim().split(" ");
 			String key = lemma + " | " + target;
 
-			/*if(lemma.indexOf(',') != -1)
-				System.out.println(lemma);
-			if(source.length > 2)
-				System.out.printf("REPLACEMENT PROBLEM: %s\n", line[0]);*/
-
-			TreeSet<String> set_of_featVecs = prs_tbl.containsKey(key) ? prs_tbl.get(key)
-																	: new TreeSet<String>();
+			TreeSet<String> set_of_featVecs = prs_tbl.containsKey(key) ?
+													prs_tbl.get(key) :
+													new TreeSet<String>();
 			set_of_featVecs.add(features);
 			prs_tbl.put(key, set_of_featVecs);
 
@@ -40,8 +40,8 @@ public class PhraseTableDataExtractor {
 		for(String key : prs_tbl.keySet()) {
 
 			TreeSet<String> set_of_featVecs = prs_tbl.get(key);
-			String lemma = key.split(" \\| ")[0]; 
-			String _class = set_of_featVecs.toArray()[0].toString(); /*$%^&*/
+			String lemma = key.split(" \\| ")[0].replaceAll(",", " ");/*$%^&*/ 
+			String _class = set_of_featVecs.toArray()[0].toString();
 			int disc_class = -1;
 
 			if(discrete_class_associations.containsKey(_class))
@@ -52,16 +52,25 @@ public class PhraseTableDataExtractor {
 			}
 
 			for(String featVec : set_of_featVecs) {
-				System.out.printf("%s,%s,%d\n", lemma, featVec, disc_class);
+				String sample = lemma + "," + featVec + "," + disc_class;
+				fw1.write(sample);
+				fw1.write("\n");
+				/*System.out.printf("%s,%s,%d\n", lemma, featVec, disc_class);*/
 			}
 
 		}
-		System.out.println("\n------------------------------\n");
+		/*System.out.println("\n------------------------------\n");*/
 		System.out.printf("NUMBER OF GROUPS: %d\nNUMBER OF CLASSES: %d\n", prs_tbl.keySet().size(), 
-														discrete_class_associations.keySet().size());
-		System.out.println("\n------------------------------\n");
-		for(String key : discrete_class_associations.keySet())
-			System.out.printf("%d ---> %s\n", discrete_class_associations.get(key), key);
+									discrete_class_associations.keySet().size());
+		/*System.out.println("\n------------------------------\n");*/
+		for(String key : discrete_class_associations.keySet()){
+			String association = discrete_class_associations.get(key) + " ---> " + key;
+			fw2.write(association);
+			fw2.write("\n");
+		}
+		
+		fw1.close();
+		fw2.close();
 	}
 
 }
